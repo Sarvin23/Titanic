8ffd928beaf8f869ff933e8abf78df4bcd5e6492
Sarvin23 <sarvingunasekar717@gmail.com>
2024-01-20 20:25:56 +0100

    Extract Title from Name,convert variable "Pclass" and extract information from Cabin

---
+++ Preprocess.R
@@ -0,0 +1,21 @@
+# Read the dataset dataset
+dataset <- read.csv("titanic.csv", stringsAsFactors = FALSE)
+
+# Extract title from the Name variable
+dataset$Title <- gsub('(.*, )|(\\..*)', '', dataset$Name)
+dataset$Title <- gsub('(Mlle|Mme|Ms|Miss|Lady|Countess|Dona)', 'Miss', dataset$Title)
+dataset$Title <- gsub('(Mrs)', 'Mrs', dataset$Title)
+dataset$Title <- gsub('(Master|Sir|Rev|Major|Col|Capt|Don|Jonkheer|Dr|)', 'Mr', dataset$Title)
+dataset$Name <- gsub("(Mlle.|Mme.|Ms.|Miss.|Lady.|Countess.|Dona.|Mrs.|Master.|Sir.|Rev.|Major.|Col.|Capt.|Don.|Jonkheer.|Dr.|Mr.)","",dataset$Name)
+
+
+# Convert variables to factors
+dataset$Pclass <- factor(dataset$Pclass, ordered = TRUE)
+
+# Extract information from Cabin variable
+dataset$Side <- ifelse(grepl("\\.", dataset$Cabin), ifelse(as.integer(gsub("[^0-9]", "", dataset$Cabin)) %% 2 == 0, "Backbord", "Steuerbord"), NA)
+dataset$Deck <- substr(dataset$Cabin, 1, 1)
+dataset$Deck[dataset$Deck == ","] <- NA
+
+# Save as preprocessed.R file
+write.csv(dataset, "https://github.com/Sarvin23/Titanic/titanic_preprocessed.csv", row.names = FALSE)
