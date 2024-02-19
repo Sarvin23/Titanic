b6dde9607cead24a4bd707cabfe2ccc1d0cf8795
Goltan Norozi <goltan200@gmail.com>
2024-02-19 18:51:10 +0100

    aufgabe2

--- Script2.R
+++ Script2.R
@@ -49,14 +49,106 @@ cat_vars <-  detect_cat_variables(data)
 print(cat_vars)
 
 
+
+
+
+# Funktion zur Berechnung geeigneter deskriptiver bivariater Statistiken für 
+# den Zusammenhang zwischen zwei kategorialen Variablen.
+# Berechnet und gibt geeignete deskriptive bivariate Statistiken
+# fuer den Zusammenhang zwischen zwei kategorialen Variablen aus.
+
+# Berechnet geeignete deskriptive bivariate Statistiken für den Zusammenhang
+# zwischen zwei kategorialen Variablen.
+
+# input: var1 Die erste kategoriale Variable.
+#        var2 Die zweite kategoriale Variable.
+# output: Eine Zusammenfassung der berechneten deskriptiven bivariaten 
+#         Statistiken.
+
+calculate_bivariate_stats_categorical <- function(var1, var2) {
+  # Kreuztabelle erstellen
+  contingency_table <- table(var1, var2)
+  
+  # Chi-Quadrat-Test durchfuehren
+  chi_sq_test <- chisq.test(contingency_table)
+  
+  # Phi-Koeffizient berechnen
+  phi_coefficient <- sqrt(chi_sq_test$statistic / sum(contingency_table))
+  
+  # Kontingenzkoeffizienten berechnen
+  contingency_coefficient <- sqrt(chi_sq_test$statistic / 
+                                    (chi_sq_test$statistic + length(var1)))
+  
+  # Zusammenfassung der Ergebnisse
+  result_summary <- list(
+    "Chi-Quadrat-Test" = chi_sq_test,
+    "Phi-Koeffizient" = phi_coefficient,
+    "Kontingenzkoeffizient" = contingency_coefficient
+  )
+  
+  return(result_summary)
+}
+
+
+
+
+# Funktion zur Berechnung geeigneter deskriptiver bivariater Statistiken 
+# fuer den Zusammenhang zwischen einer metrischen und einer dichotomen Variablen
+# Berechnet und gibt geeignete deskriptive bivariate Statistiken fuer den 
+# Zusammenhang zwischen einer metrischen und einer dichotomen Variablen aus
+
+# Berechnet geeignete deskriptive bivariate Statistiken fuer den Zusammenhang 
+# zwischen einer metrischen und einer dichotomen Variablen.
+
+# input: metric_var Die metrische Variable.
+#        dichotomous_var Die dichotome Variable.
+# otput:Eine Zusammenfassung der berechneten deskriptiven bivariaten Statistiken
+
+calculate_bivariate_stats_metric_dichotomous <- function(metric_var, 
+                                                         dichotomous_var) {
+  # Mittelwert und Standardabweichung fuer jede Kategorie der dichotomen
+  # Variable berechnen.
+  bivariate_stats <- tapply(metric_var, dichotomous_var, 
+                            function(x) c(Mittelwert = mean(x), SD = sd(x)))
+  
+  # Zusammenfassung der Ergebnisse
+  return(bivariate_stats)
+}
+
+
+
+
+# Funktion zur Erstellung einer geeigneten Visualisierung von drei oder
+# vier kategorialen Variablen
+# Erstellt und gibt eine geeignete Visualisierung von drei oder vier 
+# kategorialen Variablen aus
+
+# Erstellt eine geeignete Visualisierung von drei oder vier 
+# kategorialen Variablen.
+
+# input: var1 Die erste kategoriale Variable.
+#        var2 Die zweite kategoriale Variable.
+#        var3 Die dritte kategoriale Variable.
+#        var4 (Optional) Die vierte kategoriale Variable.
+# output: Eine visuelle Darstellung der kategorialen Variablen
+
+visualize_categorical_variables <- function(var1, var2, var3, var4 = NULL) {
+  if(is.null(var4)) {
+    mosaicplot(table(var1, var2, var3), main = "Mosaikdiagramm")
+  } else {
+    mosaicplot(table(var1, var2, var3, var4), main = "Mosaikdiagramm")
+  }
+}
+
+
+
+
+
 # Internal function to remove missing values from a vector
 # Remove missing values from a vector
-
 # This function removes missing values from a vector.
-
 # input: x A vector
 # output: A vector without missing values
-
 
 remove_missing_values <- function(x) {
   return(x[!is.na(x)])
